537c736985920d6fc1eedc8203827544102a2c05
MICHAUD <PM@MacBook-Pro-de-MICHAUD.local>
2019-07-11 18:49:41 +0200

    adding example test2.s

---
+++ resources/vm_champs/tests/test2.s
@@ -0,0 +1,15 @@
+.name "stayin' alive"
+.comment "Ha, Ha, Ha, stayiiiiin' aliiiiiiiiiive"
+
+ld %4294967295, r2
+st       r1,             5
+st r1,513        #eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee;32               ;45 #wewe
+st r1, 513#32
+st r1  ,  513#32
+st r1  ,513#32
+st r1,513 #32
+st r1, 513 #32
+st r1  ,  513 #32
+st r1  ,513 #32
+st r1, -8
+st r1, 1
