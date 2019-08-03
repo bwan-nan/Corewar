8080b068ec1d052313bbbba80260c45a3490f3a8
Benjamin WAN NAN WO <bwan-nan@student.42.fr>
2019-08-03 02:38:18 +0200

    New folder for unit test scripts (all scripts have to be executed in root folder, i.e.: ./script/split_files.sh)

--- scripts/comb.sh
+++ scripts/comb.sh
@@ -7,11 +7,11 @@ UNDERLINE=$(tput smul)
 RED=$(tput setaf 1)
 
 
-validDirectory="../vm_champs/Valid_files"
-invalidDirectory="../vm_champs/Invalid_files"
+validDirectory="Valid_files"
+invalidDirectory="Invalid_files"
 
 if [ ! -d "$validDirectory" ] || [ ! -d "$invalidDirectory" ] ; then
-	./split_files.sh
+	./scripts/split_files.sh
 fi
 
 if [[ "$1" == "clean" ]] && [[ "$2" == "" ]] ; then
@@ -19,7 +19,7 @@ if [[ "$1" == "clean" ]] && [[ "$2" == "" ]] ; then
 	exit 1
 fi
 
-if [ ! -f "../corewar" ] ; then
+if [ ! -f "corewar" ] ; then
 	echo "You should compile first..."
 	exit -1
 fi
@@ -28,18 +28,17 @@ printf "${BRIGHT}${POWDER_BLUE}Looking for differences between Zaz's VM and ours
 
 
 if [[ "$1" != "" ]] && [[ -f $validDirectory/zaz_cor_files/$1 ]] ; then
-		echo 1
 		player1=$validDirectory/zaz_cor_files/$1
 		for player2 in $(find $validDirectory/zaz_cor_files -type f -name "*.cor")
 		do
 			if [ -f "verbosity" ] ; then
 				rm verbosity
 			fi
-			../vm_champs/corewar -v 2 $player1 $player2 > verbosity
+			./vm_champs/corewar -v 2 $player1 $player2 > verbosity
 			last_cycle=`tail -n 2 verbosity | head -n 1 | grep -Eo "\d+"`
 			last_cycle=$((last_cycle-1))
-			../corewar -d $last_cycle $player1 $player2 | grep -A300 0x0000 | sed 's/ $//g' > a
-			../vm_champs/corewar -d $last_cycle $player1 $player2 | grep -A300 0x0000 | sed 's/ $//g' > b
+			./corewar -d $last_cycle $player1 $player2 | grep -A300 0x0000 | sed 's/ $//g' > a
+			./vm_champs/corewar -d $last_cycle $player1 $player2 | grep -A300 0x0000 | sed 's/ $//g' > b
 			DIFF=`diff a b`
 			if [[ "$DIFF" ]] ; then
 				printf "${BRIGHT}${WHITE}$(basename $player1 | rev | cut -c 5- | rev)   VS   %25s %5c ${RED}KO\n${NORMAL}" "$(basename $player2 | rev | cut -c 5- | rev)" ':'
@@ -55,12 +54,11 @@ else
 			if [ -f "verbosity" ] ; then
 				rm verbosity
 			fi
-			../vm_champs/corewar -v 2 $player1 $player2 > verbosity
-			echo 2
+			./vm_champs/corewar -v 2 $player1 $player2 > verbosity
 			last_cycle=`tail -n 2 verbosity | head -n 1 | grep -Eo "\d+"`
 			last_cycle=$((last_cycle-1))
-			../corewar -d $last_cycle $player1 $player2 | grep -A300 0x0000 | sed 's/ $//g' > a
-			../vm_champs/corewar -d $last_cycle $player1 $player2 | grep -A300 0x0000 | sed 's/ $//g' > b
+			./corewar -d $last_cycle $player1 $player2 | grep -A300 0x0000 | sed 's/ $//g' > a
+			./vm_champs/corewar -d $last_cycle $player1 $player2 | grep -A300 0x0000 | sed 's/ $//g' > b
 			DIFF=`diff a b`
 			if [[ "$DIFF" ]] ; then
 				printf "${BRIGHT}${WHITE}$(basename $player1 | rev | cut -c 5- | rev)   VS   %25s %5c ${RED}KO\n${NORMAL}" "$(basename $player2 | rev | cut -c 5- | rev)" ':'
--- scripts/diff_cor.sh
+++ scripts/diff_cor.sh
@@ -6,11 +6,12 @@ GREEN=$(tput setaf 2)
 UNDERLINE=$(tput smul)
 RED=$(tput setaf 1)
 
-validDirectory="../vm_champs/Valid_files"
-invalidDirectory="../vm_champs/Invalid_files"
+validDirectory="Valid_files"
+invalidDirectory="Invalid_files"
 
 if [[ "$1" == "clean" ]] && [[ "$2" == "" ]] ; then
 	rm a b verbosity *.txt
+	rm -rf $validDirectory $invalidDirectory
 	exit 1
 fi
 
@@ -19,23 +20,21 @@ if [[ "$1" == "" ]] || [[ "$2" == "" ]] ; then
 	exit -1
 fi
 
-if [ ! -f "../corewar" ] ; then
+if [ ! -f "corewar" ] ; then
 	echo "You should compile first..."
 	exit -1
 fi
 
 if [ ! -d "$validDirectory" ] || [ ! -d "$invalidDirectory" ] ; then
-	./split_files.sh
+	./scripts/split_files.sh
 fi
 
 printf "${BRIGHT}${POWDER_BLUE}Looking for differences between Zaz's VM and ours....\n${NORMAL}"
 
-#for file in $(find $validDirectory/our_cor_files -type f -name '*.s')
-#do
 	if [ -f "verbosity" ] ; then
 		rm verbosity
 	fi
-	../vm_champs/corewar -v 2 $1 $2 > verbosity
+	./vm_champs/corewar -v 2 $1 $2 > verbosity
 	cycles=`tail -n 2 verbosity | head -n 1 | grep -Eo "\d+"`
 	cycles=$(($cycles-1))
 	for ((i=1; i<$cycles; i+=1000))
@@ -47,8 +46,8 @@ printf "${BRIGHT}${POWDER_BLUE}Looking for differences between Zaz's VM and ours
 		if [ -f "b" ] ; then
 			rm b
 		fi
-		../corewar -d $i $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > a
-		../vm_champs/corewar -d $i $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > b
+		./corewar -d $i $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > a
+		./vm_champs/corewar -d $i $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > b
 		DIFF=`diff a b`
 		ko=0;
 		if [[ "$DIFF" ]] ; then
@@ -70,13 +69,13 @@ printf "${BRIGHT}${POWDER_BLUE}Looking for differences between Zaz's VM and ours
 			if [ -f "b" ] ; then
 				rm b
 			fi
-			../corewar -d $j $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > a
-			../vm_champs/corewar -d $j $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > b
+			./corewar -d $j $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > a
+			./vm_champs/corewar -d $j $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > b
 			DIFF=`diff a b`
 			ko=0;
 			if [[ "$DIFF" ]] ; then
-				../vm_champs/corewar -d $j -v 4 $1 $2 > cycle_N.txt
-				../vm_champs/corewar -d $(($j-1)) -v 4 $1 $2 > cycle_N-1.txt
+				./vm_champs/corewar -d $j -v 4 $1 $2 > cycle_N.txt
+				./vm_champs/corewar -d $(($j-1)) -v 4 $1 $2 > cycle_N-1.txt
 				diff cycle_N.txt cycle_N-1.txt | grep -v 0x | grep P > cycle$j.txt
 				rm cycle_N.txt
 				rm cycle_N-1.txt
@@ -88,8 +87,8 @@ printf "${BRIGHT}${POWDER_BLUE}Looking for differences between Zaz's VM and ours
 		done
 	elif [[ "$i" > "$cycles" ]] ; then
 		printf "Comparing the 2 VMs for cycle $cycles (last cycle):"
-		../corewar -d $cycles $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > a
-		../vm_champs/corewar -d $cycles $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > b
+		./corewar -d $cycles $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > a
+		./vm_champs/corewar -d $cycles $1 $2 | grep -A300 0x0000 | sed 's/ $//g' > b
 		DIFF=`diff a b`
 		ko=0;
 		if [[ "$DIFF" ]] ; then
@@ -99,4 +98,3 @@ printf "${BRIGHT}${POWDER_BLUE}Looking for differences between Zaz's VM and ours
 			printf "${BRIGHT}${WHITE}${GREEN}%5s\n${NORMAL}" "OK"
 		fi
 	fi
-#done
